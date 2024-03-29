61018f970a8698d4a3c1dcbc49861a08e6aa756f
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2024-03-29 18:31:19 +0200

    .

--- control.lua
+++ control.lua
@@ -1,11 +1,39 @@
 local radius = 10
 local tick_interval = 60
 
+local function transfer_items_from_container_to_wagons(containers, wagons)
+    for _, container in pairs(containers) do
+        if container.type == "container" then
+            local items = container.get_inventory(defines.inventory.chest).get_contents()
+
+            for item, count in pairs(items) do
+                local total_inserted = 0
+
+                for _, wagon in pairs(wagons) do
+                    if wagon.type == "cargo-wagon" then
+                        local inserted = wagon.get_inventory(defines.inventory.cargo_wagon).insert({name = item, count = count - total_inserted})
+
+                        total_inserted = total_inserted + inserted
+
+                        if total_inserted == count then
+                            break
+                        end
+                    end
+                end
+
+                if total_inserted > 0 then
+                    container.get_inventory(defines.inventory.chest).remove({name = item, count = total_inserted})
+                end
+            end
+        end
+    end
+end
+
 local function on_nth_tick(event)
     local surfaces = game.surfaces
 
     for _, surface in pairs(surfaces) do
-        local radars = surface.find_entities_filtered{name = "gun-turret"}
+        local radars = surface.find_entities_filtered{name = "samll-electric-pole"}
 
         for _, radar in pairs(radars) do
             local center_position = radar.position
@@ -20,31 +48,7 @@ local function on_nth_tick(event)
                 type = "cargo-wagon"
             }
 
-            for _, container in pairs(containers) do
-                if container.type == "container" then
-                    local items = container.get_inventory(defines.inventory.chest).get_contents()
-
-                    for item, count in pairs(items) do
-                        local total_inserted = 0
-
-                        for _, wagon in pairs(wagons) do
-                            if wagon.type == "cargo-wagon" then
-                                local inserted = wagon.get_inventory(defines.inventory.cargo_wagon).insert({name = item, count = count - total_inserted})
-
-                                total_inserted = total_inserted + inserted
-
-                                if total_inserted == count then
-                                    break
-                                end
-                            end
-                        end
-
-                        if total_inserted > 0 then
-                            container.get_inventory(defines.inventory.chest).remove({name = item, count = total_inserted})
-                        end
-                    end
-                end
-            end
+            transfer_items_from_container_to_wagons(containers, wagons)
         end
     end
 end
