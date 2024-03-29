6c4ce4b38c5cc588518c3e34d4aa42ae7d7e5358
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2024-03-29 18:24:19 +0200

    .

--- control.lua
+++ control.lua
@@ -1,26 +1,38 @@
 local radius = 10
-local tick_interval = 60  
+local tick_interval = 60
+
 local function on_nth_tick(event)
     local surfaces = game.surfaces
-    
+
     for _, surface in pairs(surfaces) do
-        
         local radars = surface.find_entities_filtered{name = "gun-turret"}
-            
+
         for _, radar in pairs(radars) do
-            
             local center_position = radar.position
-            
-            local entities = surface.find_entities_filtered{
+
+            local containers = surface.find_entities_filtered{
                 area = {{center_position.x - radius, center_position.y - radius}, {center_position.x + radius, center_position.y + radius}},
                 type = "container"
             }
-            
-            for _, entity in pairs(entities) do
-                
-                if entity.type == "container" then
-                    
-                    entity.clear_items_inside()
+
+            local wagons = surface.find_entities_filtered{
+                area = {{center_position.x - radius, center_position.y - radius}, {center_position.x + radius, center_position.y + radius}},
+                type = "cargo-wagon"
+            }
+
+            for _, container in pairs(containers) do
+                if container.type == "container" then
+                    local items = container.get_inventory(defines.inventory.chest).get_contents()
+
+                    for _, wagon in pairs(wagons) do
+                        if wagon.type == "cargo-wagon" then
+                            for item, count in pairs(items) do
+                                wagon.get_inventory(defines.inventory.cargo_wagon).insert({name = item, count = count})
+                            end
+                        end
+                    end
+
+                    container.clear_items_inside()
                 end
             end
         end
