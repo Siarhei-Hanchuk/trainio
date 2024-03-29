21bd9beeb98c5a750865ca3d1cbdcf69c93a8ee7
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2024-03-29 20:33:46 +0200

    .

--- control.lua
+++ control.lua
@@ -1,8 +1,9 @@
 local tick_interval = 60
 
-local function transfer_items_from_container_to_wagons(containers, wagons)
+local function transfer_items_from_containers_to_wagons(containers, wagons)
     for _, container in pairs(containers) do
-        if container.type == "container" then
+        -- remove if
+        -- if container.type == "container" then 
             local items = container.get_inventory(defines.inventory.chest).get_contents()
 
             for item, count in pairs(items) do
@@ -24,7 +25,35 @@ local function transfer_items_from_container_to_wagons(containers, wagons)
                     container.get_inventory(defines.inventory.chest).remove({name = item, count = total_inserted})
                 end
             end
-        end
+        -- end
+    end
+end
+
+local function transfer_items_from_wagons_to_containers(containers, wagons)
+    for _, wagon in pairs(wagons) do
+        -- if wagon.type == "cargo-wagon" then
+            local items = wagon.get_inventory(defines.inventory.cargo_wagon).get_contents()
+
+            for item, count in pairs(items) do
+                local total_inserted = 0
+
+                for _, container in pairs(containers) do
+                    if container.type == "container" then
+                        local inserted = container.get_inventory(defines.inventory.chest).insert({name = item, count = count - total_inserted})
+
+                        total_inserted = total_inserted + inserted
+
+                        if total_inserted == count then
+                            break
+                        end
+                    end
+                end
+
+                if total_inserted > 0 then
+                    wagon.get_inventory(defines.inventory.cargo_wagon).remove({name = item, count = total_inserted})
+                end
+            end
+        -- end
     end
 end
 
@@ -59,13 +88,18 @@ local function on_nth_tick(event)
     local surfaces = game.surfaces
 
     for _, surface in pairs(surfaces) do
-        local small_electric_poles = surface.find_entities_filtered{name = "small-electric-pole"}
+        local loaders = surface.find_entities_filtered{name = "station-loader-in"}
 
-        for _, small_electric_pole in pairs(small_electric_poles) do
+        for _, loader in pairs(loaders) do
+            local wagons, containers = get_wagons_and_containers(loader)
+            transfer_items_from_containers_to_wagons(containers, wagons)
+        end
 
-            local wagons, containers = get_wagons_and_containers(small_electric_pole)
+        local unlodaders = surface.find_entities_filtered{name = "station-loader-out"}
 
-            transfer_items_from_container_to_wagons(containers, wagons)
+        for _, unloader in pairs(unlodaders) do
+            local wagons, containers = get_wagons_and_containers(unloader)
+            transfer_items_from_wagons_to_containers(containers, wagons)
         end
     end
 end
--- prototypes/small_electric_pole.lua
+++ prototypes/small_electric_pole.lua
@@ -1,11 +1,13 @@
 local function create_small_electric_pole(type)
     local smallElectricPole = table.deepcopy(data.raw["electric-pole"]["small-electric-pole"])
     smallElectricPole.maximum_wire_distance = 25
-    smallElectricPole.name = "small-electric-pole-" .. type
+    smallElectricPole.name = "station-loader-" .. type
+
+    smallElectricPole.supply_area_distance = 0
 
     local item = table.deepcopy(data.raw["item"]["small-electric-pole"])
-    item.name = item.name .. "-" .. type
-    item.place_result = item.place_result .. "-" .. type
+    item.name = "station-loader-" .. type
+    item.place_result = "station-loader-" .. type
 
     return smallElectricPole, item
 end
