fbdefe0a41b3e24d925f3a906cd325446104190b
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2024-03-31 19:11:56 +0300

    Vertical and Horizontal wide chest

--- prototypes/steel_chest.lua
+++ prototypes/steel_chest.lua
@@ -1,33 +1,52 @@
 local steelChest = table.deepcopy(data.raw["container"]["steel-chest"])
 
-steelChest.name = steelChest.name .. "-wide"
-steelChest.circuit_wire_max_distance = 50
-steelChest.inventory_size = steelChest.inventory_size * 6
-steelChest.collision_box = {{-3, -0.5}, {3, 0.5}}
-steelChest.selection_box = {{-3, -0.5}, {3, 0.5}}
+local function create_wide_steel_chest(box, width, height, postfix)
+    local steelChest = table.deepcopy(data.raw["container"]["steel-chest"])
+    steelChest.name = steelChest.name .. "-wide-" .. postfix
+    steelChest.circuit_wire_max_distance = 50
+    steelChest.inventory_size = steelChest.inventory_size * 6
+    steelChest.collision_box = box
+    steelChest.selection_box = box
 
-steelChest.picture = {
-    filename = "__trainio__/graphics/hr-steel-chest.png",
-    priority = "extra-high",
-    width = 64 * 6,
-    height = 80,
-    shift = {0, 0},
-    scale = 0.5,
-}
+    steelChest.picture = {
+        filename = "__trainio__/graphics/steel-chest-" .. postfix .. ".png",
+        priority = "extra-high",
+        width = width,
+        height = height,
+        shift = {0, 0},
+        scale = 0.5,
+    }
 
-local steelChestRecipe = table.deepcopy(data.raw["recipe"]["steel-chest"])
-steelChestRecipe.name = steelChestRecipe.name .. "-wide"
-steelChestRecipe.result = steelChestRecipe.result .. "-wide"
+    local steelChestRecipe = table.deepcopy(data.raw["recipe"]["steel-chest"])
+    steelChestRecipe.name = steelChestRecipe.name .. "-wide-" .. postfix
+    steelChestRecipe.result = steelChestRecipe.result .. "-wide-" .. postfix
 
-local steelChestItem = table.deepcopy(data.raw["item"]["steel-chest"])
-steelChestItem.name = steelChestItem.name .. "-wide"
-steelChestItem.place_result = steelChestItem.place_result .. "-wide"
+    local steelChestItem = table.deepcopy(data.raw["item"]["steel-chest"])
+    steelChestItem.name = steelChestItem.name .. "-wide-" .. postfix
+    steelChestItem.place_result = steelChestItem.place_result .. "-wide-" .. postfix
+    steelChestItem.icon = "__trainio__/graphics/steel-chest-" .. postfix .. "-icon.png"
+
+    return steelChest, steelChestRecipe, steelChestItem
+end
+
+local box = {{-3, -0.5}, {3, 0.5}}
+local steelChestH, steelChestRecipeH, steelChestItemH = create_wide_steel_chest(box, 64 * 6, 80, "h")
+
+
+local box = {{-0.5, -3}, {0.5, 3}}
+local steelChestV, steelChestRecipeV, steelChestItemV = create_wide_steel_chest(box, 64, 80 * 6, "v")
 
 data:extend{
-    steelChest, steelChestItem, steelChestRecipe,
+    steelChestH, steelChestRecipeH, steelChestItemH,
+    steelChestV, steelChestRecipeV, steelChestItemV,
 }
 
 table.insert(
     data.raw["technology"]["steel-processing"].effects,
-    {type = "unlock-recipe", recipe = steelChestRecipe.name}
+    {type = "unlock-recipe", recipe = steelChestRecipeH.name}
+)
+
+table.insert(
+    data.raw["technology"]["steel-processing"].effects,
+    {type = "unlock-recipe", recipe = steelChestRecipeV.name}
 )
