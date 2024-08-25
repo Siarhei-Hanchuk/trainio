37749026c3d8e96ccac5a75b244848f4599fecf9
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2024-08-25 17:00:11 +0300

    Finish Refactoring

--- scripts/transfer.lua
+++ scripts/transfer.lua
@@ -23,24 +23,6 @@ local function transfer_items_from_to(source_inventories, destination_inventorie
 end
 
 
-local function transfer_items_from_station_to_train(station)
-    local stationStorage = global.linked_chests[station.unit_number]
-    local inventories = {}
-
-    local train = station.get_stopped_train()
-    if train then
-        for _, wagon in pairs(train.cargo_wagons) do
-            local wagon_inventory = wagon.get_inventory(defines.inventory.cargo_wagon)
-            table.insert(inventories, wagon_inventory)
-        end
-    end
-
-    transfer_items_from_to(stationStorage.get_inventory(defines.inventory.chest), inventories)
-end
-
-
-
-
 local function get_inventories(factory, type)
     if factory.name == "assembling-machine-1" or factory.name == "assembling-machine-2" then
         if type == "from-train" then
@@ -109,12 +91,53 @@ local function find_factories_around_train_stop(train_stop, station_type)
 end
 
 
+local function transfer_items_from_station_to_train(station)
+    local stationStorage = global.linked_chests[station.unit_number]
+    local inventories = {}
+
+    local train = station.get_stopped_train()
+    if train then
+        for _, wagon in pairs(train.cargo_wagons) do
+            local wagon_inventory = wagon.get_inventory(defines.inventory.cargo_wagon)
+            table.insert(inventories, wagon_inventory)
+        end
+
+        transfer_items_from_to({stationStorage.get_inventory(defines.inventory.chest)}, inventories)
+    end
+end
+
+
+local function transfer_items_from_train_to_station(station)
+    local stationStorage = global.linked_chests[station.unit_number]
+    local inventories = {}
+
+    local train = station.get_stopped_train()
+    if train then
+        for _, wagon in pairs(train.cargo_wagons) do
+            local wagon_inventory = wagon.get_inventory(defines.inventory.cargo_wagon)
+            table.insert(inventories, wagon_inventory)
+        end
+
+        transfer_items_from_to(inventories, {stationStorage.get_inventory(defines.inventory.chest)})
+    end
+end
+
+
 local function transfer_items_from_factories_to_station(station)
     local stationStorage = global.linked_chests[station.unit_number]
 
     -- TODO: use cache
     factories_inventories = find_factories_around_train_stop(station, "to-train")
     transfer_items_from_to(factories_inventories, {stationStorage.get_inventory(defines.inventory.chest)})
+end
+
+
+local function transfer_items_from_station_factories(station)
+    local stationStorage = global.linked_chests[station.unit_number]
+
+    -- TODO: use cache
+    factories_inventories = find_factories_around_train_stop(station, "to-train")
+    transfer_items_from_to({stationStorage.get_inventory(defines.inventory.chest)}, factories_inventories)
 end
 
 
@@ -129,14 +152,11 @@ script.on_nth_tick(60, function(event)
             transfer_items_from_station_to_train(loader)
         end
 
-        -- local unlodaders = surface.find_entities_filtered{name = "train-stop-from-train"}
+        local unlodaders = surface.find_entities_filtered{name = "train-stop-from-train"}
 
-        -- for _, unloader in pairs(unlodaders) do
-        --     local wagons, containers = get_wagons_and_containers(unloader)
-        --     transfer_items_from_to(wagons, containers)
-
-        --     factories = find_factories_around_train_stop(unloader)
-        --     transfer_items_from_to(containers, factories, "unloading")
-        -- end
+        for _, unloader in pairs(unlodaders) do
+            transfer_items_from_train_to_station(loader)
+            transfer_items_from_station_factories(unloader)
+        end
     end
 end)
